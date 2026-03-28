bbf82fa841b3a0671037a5a4b527d44d730245ac
aso-iris <133760417+aso-iris@users.noreply.github.com>
2026-03-28 11:03:44 +0000

    Fix MainActivity and add videos folder

--- android/app/src/main/kotlin/com/naijamart/tech/MainActivity.kt
+++ android/app/src/main/kotlin/com/naijamart/tech/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.naijamart.tech
 
-//import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-class MainActivity: FlutterFragmentActivity() {
-}
+class MainActivity: FlutterActivity()
